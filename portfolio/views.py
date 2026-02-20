e929b67dc88bb635497ee4ffe1dcc19ba63228c3
brady Fresnel <mokumabrady13@gmail.com>
2026-02-20 13:25:55 +0100

    résolution de compatibilité avec python 3.12.

--- portfolio/views.py
+++ portfolio/views.py
@@ -6,7 +6,6 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import authenticate, login
-from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import ProjectSerializer, CategorySerializer, TechnologySerializer, ContactSerializer
 
 # Vues existantes...
